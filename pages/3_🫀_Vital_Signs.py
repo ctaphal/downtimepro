fcfd913a7c91d217e5903d7a1685a0d90dce1b51
ctaphal <67525176+ctaphal@users.noreply.github.com>
2023-10-09 23:36:19 +0000

    Updated vital signs pg

--- pages/3_🫀_Vital_Signs.py
+++ pages/3_🫀_Vital_Signs.py
@@ -1,50 +1,61 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+from datetime import datetime
+import pytz
+from pytz import timezone
 
+st.set_page_config(page_title="🫀 Vital Signs")
+st.title("Vital Signs")
+st.sidebar.header("")
 
-def pt_dem() -> None:
-    st.subheader("Personal Information")
-    fname = st.text_input("First name")
-    lname = st.text_input("Last name")
-    dob = st.text_input("DOB")
-    ethnicity_race = st.text_input("Ethnicity/Race")
-    age = st.text_input("Age")
-    gender = st.text_input("Gender")
-    height = st.text_input("Height (in)")
-    weight = st.text_input("Weight (lbs)")
+def main():
 
-    ## next line is a spacer
-    st.markdown("")
-    st.subheader("Address")
-    st_address = st.text_input("Street Address")
-    city = st.text_input("City")
-    zip_code = st.text_input("Zip Code")
+    # set defaults
+    date = ""
+    time = ""
+    hr = ""
+    bp = ""
+    o2 = ""
+    resp = ""
+    notes = ""
 
-    ## next line is a spacer
-    st.markdown("")
-    st.subheader("Insurance")
-    insurance_provider = st.text_input("Insurance Provider")
-    insurance_num = st.text_input("Insurance Member ID")
+    # take inputs
+    st.write('**Enter patient vital signs below and click "Enter" to save.**')
+    st.write('(Must press enter after every text entry to save values)')
+    st.markdown("""---""")
 
-st.set_page_config(page_title="Patient Information", page_icon="🧑")
-st.markdown("# Patient Information")
+    choice = st.radio("Use current date and time or enter manually?", ("Use current date and time", "Enter manually"))
 
-pt_dem()
+    if (choice=="Enter manually"):
+        date = st.text_input("Date (MM/DD/YYYY)")
+        time = st.text_input("Time (Military)")
+    elif (choice=="Use current date and time"):
+        now = datetime.now(timezone("Canada/Eastern"))
+        date = now.strftime("%m/%d/%Y")
+        time = now.strftime("%H:%M")
+        st.write("Today's Date: ", date)
+        st.write("Current Time: ", time)
+
+    hr = st.text_input("Heart rate")
+    bp = st.text_input("Blood Pressure")
+    o2 = st.text_input("Sp02")
+    resp = st.text_input("Respirations")
+    notes = st.text_area("Optional additional notes")
+    if (st.button("Enter")):
+        st.markdown("""---""")
+        if date!="":
+            st.write("Date: ", date)
+        if time!="":
+            st.write("Time: ", time)
+        if hr!="":
+            st.write("HR: ", hr)
+        if bp!="":
+            st.write("BP: ", bp)
+        if o2!="":
+            st.write("SpO2: ", o2)
+        if resp!="":
+            st.write("Respirations: ", resp)
+        if notes!="":
+            st.write("Additional Notes: ", notes)
+
+if __name__ == "__main__":
+    main()
